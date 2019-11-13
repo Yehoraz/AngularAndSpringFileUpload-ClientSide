d112bdf80f09c790a665b3be4ff6a83dc7d86d99
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-11-14 01:25:15 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularAndSpring';
+
+  private file = null;
+
+  constructor(private http: HttpClient) { }
+
+  private imageChoice(theEventFromHtml) {
+    this.file = theEventFromHtml.target.files[0];
+  }
+
+  private sendFile() {
+
+    if (this.file !== null) {
+
+      const formdata: FormData = new FormData();
+
+      formdata.append('file', this.file);
+
+      this.http.post("http://localhost:8383/addFile", formdata, { observe: 'response', responseType: 'text'}).subscribe();
+    }
+  }
 }
